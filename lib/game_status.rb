bf20c474e06e596cb9ec024a6aba03212030c1b9
Betsy Sallee <ensallee@gmail.com>
2018-03-01 18:05:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,6 +27,8 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return combination
     else
       false
     end
